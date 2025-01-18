333cdebf3fe7135d3c7c30975a6407665da7b4c2
t9tieanh <phama9162@gmail.com>
2025-01-18 19:35:00 +0700

    update mail service + otp

--- app/src/main/java/com/example/login/LoginSuccess.java
+++ app/src/main/java/com/example/login/LoginSuccess.java
@@ -9,6 +9,6 @@ public class LoginSuccess extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_success);
-
     }
 }
+
--- demo/src/main/java/login/com/demo/controller/AccountController.java
+++ demo/src/main/java/login/com/demo/controller/AccountController.java
@@ -24,16 +24,6 @@ import java.util.List;
 public class AccountController {
     AccountService accountService;
 
-    @PostMapping
-    public ResponseEntity<Boolean> checkLogin(@RequestBody AccountDTO accountDTO) {
-        String username = accountDTO.getUsername();
-        String password = accountDTO.getPassword();
-
-        boolean validLogin = accountService.findAccount(username, password);
-        var t = ResponseEntity.ok(validLogin);
-        return t;
-    }
-
     @PostMapping(("/login"))
     public ResponseEntity<AccountDTO> Login(@RequestBody AccountDTO accountDTO) throws AccountNotFoundException {
         if (accountDTO == null || accountDTO.getUsername() == null || accountDTO.getPassword() == null)
@@ -56,11 +46,10 @@ public class AccountController {
         OTP codeOTP = new OTP(6);
         request.getSession().setAttribute(OTPConstant.OTP, codeOTP);
 
-        var check = request.getSession().getAttribute(OTPConstant.OTP);
 
         MailUtils mailUtils = new MailUtils();
-        if (mailUtils.sendEmail("phama9162@gmail.com", email , MailConfirmOTPConstant.SUBJECT, MailConfirmOTPConstant.CONTENT)) {
-            return ResponseEntity.ok(new MessageDTO("Gửi OTP thành công !","success"));
+        if (mailUtils.sendEmail("phama9162@gmail.com", email , MailConfirmOTPConstant.SUBJECT, MailConfirmOTPConstant.CONTENT + codeOTP.getOtp())) {
+            return ResponseEntity.ok(new MessageDTO(MailConfirmOTPConstant.REPONSE,"success"));
         } else  throw new Exception("Chưa thể gửi OTP");
     }
 
@@ -71,9 +60,9 @@ public class AccountController {
             return ResponseEntity.badRequest().body(null); // Trả về mã 400 nếu dữ liệu không hợp lệ
 
         // đưa otp vào session
-        var codeOTP = request.getSession().getAttribute(OTPConstant.OTP);
+        OTP codeOTP = (OTP) request.getSession().getAttribute(OTPConstant.OTP);
 
-        if (code.equals("codeOTP.getOtp()")) {
+        if (code.equals(codeOTP.getOtp())) {
             return ResponseEntity.ok(new MessageDTO("Tạo tài khoản thành công !","success",true));
         } else
             return ResponseEntity.ok(new MessageDTO("OTP không đúng!","failure",false));
